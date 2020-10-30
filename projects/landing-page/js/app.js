a35460b3ab6d0cad0adf9f52fee9a72fe1c2a9be
Silvia-Fado <silviafado@gmail.com>
2020-10-30 14:08:22 +0100

    last

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -14,19 +14,19 @@
 */
 
 /* Define Global Variables */
-var sections=['Section 1','Section 2','Section 3','Section 4'];
-var buttons=['Button 1','Button 2','Button 3','Button 4'];
-var ul=document.getElementById('navbar__list');
+let sections=['Section 1','Section 2','Section 3','Section 4'];
+let buttons=['Button 1','Button 2','Button 3','Button 4'];
+const ul=document.getElementById('navbar__list');
 const sections2 = document.querySelectorAll("section");
 
 
 /* Main functions */
 /* Building the navigation bar */
 function buildNavBar(){
-  for (var i=0;i<sections.length;i++){
-    var section=sections[i];
-    var li=document.createElement('li');
-    var button=document.createElement('button');   
+  for (let i=0;i<sections.length;i++){
+    let section=sections[i];
+    const li=document.createElement('li');
+    const button=document.createElement('button');   
     li.appendChild(button);
     button.textContent=section;
     ul.appendChild(li);
@@ -52,9 +52,9 @@ function classActive() {
 
 /* Start Helper Functions */
 /* Hide navbar when scrolling down */
-var prevScrollpos = window.pageYOffset;
+let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
+  let currentScrollPos = window.pageYOffset;
   if (prevScrollpos < currentScrollPos) {
     ul.style.visibility = 'hidden';
   } else {
@@ -66,47 +66,47 @@ window.onscroll = function() {
 
 /* Styling */
 /* Styling Navigation */
-var styleBar=document.getElementById('navbar__list');
+const styleBar=document.getElementById('navbar__list');
 styleBar.style.cssText='background-color: rgba(136,203,171,1)'; 
 
 /* Styling buttons */
-var styleButton1=document.getElementById('Button 1');
+const styleButton1=document.getElementById('Button 1');
 styleButton1.style.cssText='background-color: rgba(0,13,60,1); color: white; font-size: 1em';
 
-var styleButton2=document.getElementById('Button 2');
+const styleButton2=document.getElementById('Button 2');
 styleButton2.style.cssText='background-color: #425d6b; color: white; font-size: 1em';
 
-var styleButton3=document.getElementById('Button 3');
+const styleButton3=document.getElementById('Button 3');
 styleButton3.style.cssText='background-color: #688e89; color: white; font-size: 1em';
 
-var styleButton4=document.getElementById('Button 4');
+const styleButton4=document.getElementById('Button 4');
 styleButton4.style.cssText='background-color: rgb(136,203,171); color: white; font-size: 1em';
 
 
 /* Events */
 /* Scrolling events */
-var scroll1=document.getElementById('Section 1');
+const scroll1=document.getElementById('Section 1');
 scroll1.addEventListener('click', function(){
   section1.scrollIntoView({
     behavior:'smooth'
   }); 
 })
 
-var scroll2=document.getElementById('Section 2');
+const scroll2=document.getElementById('Section 2');
 scroll2.addEventListener('click', function(){
   section2.scrollIntoView({
     behavior:'smooth'
   });
 })
 
-var scroll3=document.getElementById('Section 3');
+const scroll3=document.getElementById('Section 3');
 scroll3.addEventListener('click', function(){
   section3.scrollIntoView({
     behavior:'smooth'
   });
 })
 
-var scroll4=document.getElementById('Section 4');
+const scroll4=document.getElementById('Section 4');
 scroll4.addEventListener('click', function(){
   section4.scrollIntoView({
     behavior:'smooth'
